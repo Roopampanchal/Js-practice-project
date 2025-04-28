394ed658044e252184f8d7685c633b87c59e6aaa
Roopam Panchal <100795493+Roopampanchal@users.noreply.github.com>
2025-04-28 09:57:36 +0000

    Maths

---
+++ 01_basic/06_number_math.js
@@ -0,0 +1,49 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100.436855); // define type
+// console.log(balance);
+// console.log(balance.toFixed(1)); // 100.0
+
+
+// console.log(balance.toString()); // convert number to string
+const numToStr = balance.toString();
+// console.log(typeof numToStr);
+
+//Using string we can use string's various methods
+
+// console.log(numToStr.length);
+
+
+const otherNum = 123.899954;
+// const otherNum2 = 0.000123;
+// console.log(otherNum.toPrecision(1));
+
+// +++++++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-9587));
+// console.log(Math.round(34340.65123));
+// console.log(Math.ceil(-7.004));
+// console.log(Math.floor(5.05));
+// console.log(Math.min(3,4,7,8,-2.5,2.6));
+// console.log(Math.max(3,4,7,8,-2.5,2.6));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+
+
+
+
+
+
+
